ec1605433231ed34269dd70bdf4a39af034eb91c
arthurvinice <arthurvinice@gmail.com>
2025-02-19 19:33:20 -0300

    Criação da função para converter os valores passados pelo usuário em booleano

--- script_html.js
+++ script_html.js
@@ -1,8 +1,26 @@
 import { duasEntMUX, quatroEntMUX, oitoEntMUX } from "./script.js";
 
 function duasMult(){
-    let e0 = document.getElementById("d-e0").value
-    let e1 = document.getElementById("d-e1").value
-    let c0 = document.getElementById("d-c0").value
-    duasEntMUX(e0,e1,c0)
+    let de0 = document.getElementById("d-e0").value
+    let de1 = document.getElementById("d-e1").value
+    let dc0 = document.getElementById("d-c0").value
+
+    
+
+    return console.log(duasEntMUX(
+        conversorBool(de0),
+        conversorBool(de1),
+        conversorBool(dc0)
+    ))
+}
+
+//funcao pra receber apenas 0 ou 1 no input
+function validacao(input) {
+    if (input.value !== "0" && input.value !== "1") {
+        input.value = "" 
+    }
+}
+//função para converter os valores numericos em boolean
+function conversorBool(value) {
+    return value === 1 || value === "1";
 }
