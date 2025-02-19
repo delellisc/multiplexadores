636f3986aafa8a887cae65bb858b27215af38d33
arthurvinice <arthurvinice@gmail.com>
2025-02-19 19:18:45 -0300

    atualização de id's das divs

--- script_html.js
+++ script_html.js
@@ -1,8 +1,8 @@
 import { duasEntMUX, quatroEntMUX, oitoEntMUX } from "./script.js";
 
 function duasMult(){
-    let e0 = document.getElementById("d-e0")
-    let e1 = document.getElementById("d-e1")
-    let c0 = document.getElementById("d-c0")
+    let e0 = document.getElementById("d-e0").value
+    let e1 = document.getElementById("d-e1").value
+    let c0 = document.getElementById("d-c0").value
     duasEntMUX(e0,e1,c0)
 }
