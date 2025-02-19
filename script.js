fc70ed66a6f56a19f0dadafb486a981a15296fbb
delellisc <delellis021@gmail.com>
2025-02-19 16:10:37 -0300

    Adicionando arquivos iniciais do projeto; Escrevendo funções de portas lógicas.

---
+++ script.js
@@ -0,0 +1,44 @@
+// AB
+function portaAND(entrada1, entrada2) {
+    return entrada1 && entrada2;
+}
+
+// A + B
+function portaOR(entrada1, entrada2) {
+    return entrada1 || entrada2;
+}
+
+// A'
+function portaNOT(entrada) {
+    return !entrada;
+}
+
+// (AB)'
+function portaNAND(entrada1, entrada2) {
+    return !(entrada1 && entrada2);
+    // ou:
+    // return portaNOT((portaAND(entrada1, entrada2)));
+}
+
+// (A + B)'
+function portaNOR(entrada1, entrada2) {
+    return !(entrada1 || entrada2);
+    // ou:
+    // return portaNOT((portaOR(entrada1, entrada2)));
+}
+
+// A⊕B
+// AB' + A'B
+function portaXOR(entrada1, entrada2){
+    return (entrada1 && !entrada2) || (!entrada1 && entrada2);
+    // ou:
+    // return portaOR(portaAND(entrada1, portaNOT(entrada2)), portaAND(portaNOT(entrada1), entrada2));
+}
+
+// A⊙B
+// (AB' + A'B)'
+function portaXNOR(entrada1, entrada2){
+    return !((entrada1 && !entrada2) || (!entrada1 && entrada2));
+    // ou:
+    // return portaNOT(portaOR(portaAND(entrada1, portaNOT(entrada2)), portaAND(portaNOT(entrada1), entrada2)));
+}
