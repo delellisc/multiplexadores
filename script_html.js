9ecd87c327cc73879ad8fb828d8b4fffed4e5e99
arthurvinice <arthurvinice@gmail.com>
2025-02-19 19:17:16 -0300

    importação corrigida

--- script_html.js
+++ script_html.js
@@ -1,1 +1,8 @@
-import { duasEntMUX, quatroEntMUX, oitoEntMUX } from "./script";
+import { duasEntMUX, quatroEntMUX, oitoEntMUX } from "./script.js";
+
+function duasMult(){
+    let e0 = document.getElementById("d-e0")
+    let e1 = document.getElementById("d-e1")
+    let c0 = document.getElementById("d-c0")
+    duasEntMUX(e0,e1,c0)
+}
