40b9acc8054e9d6ba8cfbe34ffdb809f7ef1d01a
delellisc <delellis021@gmail.com>
2025-02-19 20:22:38 -0300

    Corrigindo a função do MUX de 8 entradas.

--- script.js
+++ script.js
@@ -63,23 +63,19 @@ export function quatroEntMUX(ent0, ent1, ent2, ent3, cont1, cont0){
 
 /* MULTIPLEXADOR DE 8 ENTRADAS */
 export function oitoEntMUX(ent0, ent1, ent2, ent3, ent4, ent5, ent6, ent7, cont2, cont1, cont0, enable){
-    cont2 = !cont2;
-    cont1 = !cont1;
-    cont0 = !cont0;
-    /* enable = !enable; */
-    and0 = ent0 && cont0 && cont1 && cont2 && enable;
-    and1 = ent1 && !cont0 && cont1 && cont2 && enable;
-    and2 = ent2 && cont0 && !cont1 && cont2 && enable;
-    and3 = ent3 && !cont0 && !cont1 && cont2 && enable;
-    and4 = ent4 && cont0 && cont1 && !cont2 && enable;
-    and5 = ent5 && !cont0 && cont1 && !cont2 && enable;
-    and6 = ent6 && cont0 && !cont1 && !cont2 && enable;
-    and7 = ent7 && !cont0 && !cont1 && !cont2 && enable;
+    enable = !enable;
+    and0 = ent0 && !cont0 && !cont1 && !cont2 && enable;
+    and1 = ent1 && cont0 && !cont1 && !cont2 && enable;
+    and2 = ent2 && !cont0 && cont1 && !cont2 && enable;
+    and3 = ent3 && cont0 && cont1 && !cont2 && enable;
+    and4 = ent4 && !cont0 && !cont1 && cont2 && enable;
+    and5 = ent5 && cont0 && !cont1 && cont2 && enable;
+    and6 = ent6 && !cont0 && cont1 && cont2 && enable;
+    and7 = ent7 && cont0 && cont1 && cont2 && enable;
     nor0 = !(and0 || and1 || and2 || and3 || and4 || and5 || and6 || and7);
-    // z = !nor0
-    // z' = nor0
-    // z' = i*
-    return nor0;
+    // z = !nor0 = i*
+    // z' = nor0 = i*'
+    return !nor0;
 }
 
-console.log(quatroEntMUX(false, false, false, false, true, true));
+console.log(oitoEntMUX(true, false, false, false, false, false, false, false, false, false, false, false));
