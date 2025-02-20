dcc428a97997ea120166b6393fbd90b5bc5f9e0d
delellisc <delellis021@gmail.com>
2025-02-19 21:00:13 -0300

    Identificando script_html.js como um módulo para que imports sejam válidos. Adaptando o script para escutar eventos da página HTML.

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+/*************************************************************************************************/
+/* MULTIPLEXADOR DE 2 ENTRADAS */
+export function duasEntMUX(entrada0, entrada1, controle){ 
+    return (entrada0 && !controle) || (entrada1 && controle); 
+};
+
+/* MULTIPLEXADOR DE 4 ENTRADAS */
+export function quatroEntMUX(ent0, ent1, ent2, ent3, cont1, cont0){
+    and0 = ent0 && !cont0 && !cont1;
+    and1 = ent1 && cont0 && !cont1;
+    and2 = ent2 && !cont0 && cont1;
+    and3 = ent3 && cont0 && cont1;
+    // z = and0 || and1 || and2 || and3;
+    return and0 || and1 || and2 || and3;
+    // ou:
+    // return (ent0 && !cont0 && !cont1) || (ent1 && cont0 && !cont1) || (ent2 && !cont0 && cont1) || (ent3 && cont0 && cont1);
+};
+
+/* MULTIPLEXADOR DE 8 ENTRADAS */
+export function oitoEntMUX(ent0, ent1, ent2, ent3, ent4, ent5, ent6, ent7, cont2, cont1, cont0, enable){
+    enable = !enable;
+    and0 = ent0 && !cont0 && !cont1 && !cont2 && enable;
+    and1 = ent1 && cont0 && !cont1 && !cont2 && enable;
+    and2 = ent2 && !cont0 && cont1 && !cont2 && enable;
+    and3 = ent3 && cont0 && cont1 && !cont2 && enable;
+    and4 = ent4 && !cont0 && !cont1 && cont2 && enable;
+    and5 = ent5 && cont0 && !cont1 && cont2 && enable;
+    and6 = ent6 && !cont0 && cont1 && cont2 && enable;
+    and7 = ent7 && cont0 && cont1 && cont2 && enable;
+    nor0 = !(and0 || and1 || and2 || and3 || and4 || and5 || and6 || and7);
+    // z = !nor0 = i*
+    // z' = nor0 = i*'
+    return !nor0;
+}
+
+/*************************************************************************************************/
 // AB
 function portaAND(entrada1, entrada2) {
     return entrada1 && entrada2;
@@ -42,40 +78,3 @@ function portaXNOR(entrada1, entrada2){
     // ou:
     // return portaNOT(portaOR(portaAND(entrada1, portaNOT(entrada2)), portaAND(portaNOT(entrada1), entrada2)));
 }
-
-/*************************************************************************************************/
-/* MULTIPLEXADOR DE 2 ENTRADAS */
-export function duasEntMUX(entrada0, entrada1, controle){ 
-    return (entrada0 && !controle) || (entrada1 && controle); 
-};
-
-/* MULTIPLEXADOR DE 4 ENTRADAS */
-export function quatroEntMUX(ent0, ent1, ent2, ent3, cont1, cont0){
-    and0 = ent0 && !cont0 && !cont1;
-    and1 = ent1 && cont0 && !cont1;
-    and2 = ent2 && !cont0 && cont1;
-    and3 = ent3 && cont0 && cont1;
-    // z = and0 || and1 || and2 || and3;
-    return and0 || and1 || and2 || and3;
-    // ou:
-    // return (ent0 && !cont0 && !cont1) || (ent1 && cont0 && !cont1) || (ent2 && !cont0 && cont1) || (ent3 && cont0 && cont1);
-};
-
-/* MULTIPLEXADOR DE 8 ENTRADAS */
-export function oitoEntMUX(ent0, ent1, ent2, ent3, ent4, ent5, ent6, ent7, cont2, cont1, cont0, enable){
-    enable = !enable;
-    and0 = ent0 && !cont0 && !cont1 && !cont2 && enable;
-    and1 = ent1 && cont0 && !cont1 && !cont2 && enable;
-    and2 = ent2 && !cont0 && cont1 && !cont2 && enable;
-    and3 = ent3 && cont0 && cont1 && !cont2 && enable;
-    and4 = ent4 && !cont0 && !cont1 && cont2 && enable;
-    and5 = ent5 && cont0 && !cont1 && cont2 && enable;
-    and6 = ent6 && !cont0 && cont1 && cont2 && enable;
-    and7 = ent7 && cont0 && cont1 && cont2 && enable;
-    nor0 = !(and0 || and1 || and2 || and3 || and4 || and5 || and6 || and7);
-    // z = !nor0 = i*
-    // z' = nor0 = i*'
-    return !nor0;
-}
-
-console.log(oitoEntMUX(true, false, false, false, false, false, false, false, false, false, false, false));
--- script_html.js
+++ script_html.js
@@ -1,12 +1,23 @@
 import { duasEntMUX, quatroEntMUX, oitoEntMUX } from "./script.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".dois-dig button").addEventListener("click", duasMult);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const inputs = document.querySelectorAll('input[type="number"]');
+    
+    inputs.forEach(input => {
+        input.addEventListener('input', (event) => {
+            validacao(event.target);
+        });
+    });
+});
+
 function duasMult(){
     let de0 = document.getElementById("d-e0").value
     let de1 = document.getElementById("d-e1").value
     let dc0 = document.getElementById("d-c0").value
-
-    
-
     return console.log(duasEntMUX(
         conversorBool(de0),
         conversorBool(de1),
