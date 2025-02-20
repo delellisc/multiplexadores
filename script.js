de5683662c7eb976ba3487bb60e5434db27116ad
delellisc <delellis021@gmail.com>
2025-02-19 21:30:53 -0300

    Adição de formulários para MUX de 4 e 8 entradas; Coomportamento esperado é observado quando executado via terminal, porém quando executado no navegador, apresenta-se um comportamento inesperado.

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ export function duasEntMUX(entrada0, entrada1, controle){
 
 /* MULTIPLEXADOR DE 4 ENTRADAS */
 export function quatroEntMUX(ent0, ent1, ent2, ent3, cont1, cont0){
-    and0 = ent0 && !cont0 && !cont1;
-    and1 = ent1 && cont0 && !cont1;
-    and2 = ent2 && !cont0 && cont1;
-    and3 = ent3 && cont0 && cont1;
+    var and0 = ent0 && !cont0 && !cont1;
+    var and1 = ent1 && cont0 && !cont1;
+    var and2 = ent2 && !cont0 && cont1;
+    var and3 = ent3 && cont0 && cont1;
     // z = and0 || and1 || and2 || and3;
     return and0 || and1 || and2 || and3;
     // ou:
@@ -19,15 +19,15 @@ export function quatroEntMUX(ent0, ent1, ent2, ent3, cont1, cont0){
 /* MULTIPLEXADOR DE 8 ENTRADAS */
 export function oitoEntMUX(ent0, ent1, ent2, ent3, ent4, ent5, ent6, ent7, cont2, cont1, cont0, enable){
     enable = !enable;
-    and0 = ent0 && !cont0 && !cont1 && !cont2 && enable;
-    and1 = ent1 && cont0 && !cont1 && !cont2 && enable;
-    and2 = ent2 && !cont0 && cont1 && !cont2 && enable;
-    and3 = ent3 && cont0 && cont1 && !cont2 && enable;
-    and4 = ent4 && !cont0 && !cont1 && cont2 && enable;
-    and5 = ent5 && cont0 && !cont1 && cont2 && enable;
-    and6 = ent6 && !cont0 && cont1 && cont2 && enable;
-    and7 = ent7 && cont0 && cont1 && cont2 && enable;
-    nor0 = !(and0 || and1 || and2 || and3 || and4 || and5 || and6 || and7);
+    var and0 = ent0 && !cont0 && !cont1 && !cont2 && enable;
+    var and1 = ent1 && cont0 && !cont1 && !cont2 && enable;
+    var and2 = ent2 && !cont0 && cont1 && !cont2 && enable;
+    var and3 = ent3 && cont0 && cont1 && !cont2 && enable;
+    var and4 = ent4 && !cont0 && !cont1 && cont2 && enable;
+    var and5 = ent5 && cont0 && !cont1 && cont2 && enable;
+    var and6 = ent6 && !cont0 && cont1 && cont2 && enable;
+    var and7 = ent7 && cont0 && cont1 && cont2 && enable;
+    var nor0 = !(and0 || and1 || and2 || and3 || and4 || and5 || and6 || and7);
     // z = !nor0 = i*
     // z' = nor0 = i*'
     return !nor0;
--- script_html.js
+++ script_html.js
@@ -1,9 +1,6 @@
 import { duasEntMUX, quatroEntMUX, oitoEntMUX } from "./script.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".dois-dig button").addEventListener("click", duasMult);
-});
-
+// validação dos inputs
 document.addEventListener("DOMContentLoaded", () => {
     const inputs = document.querySelectorAll('input[type="number"]');
     
@@ -14,14 +11,78 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// executar MUX de duas entradas
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".dois-dig button").addEventListener("click", duasMult);
+});
+
 function duasMult(){
     let de0 = document.getElementById("d-e0").value
     let de1 = document.getElementById("d-e1").value
     let dc0 = document.getElementById("d-c0").value
+    console.log(de0, de1, dc0)
     return console.log(duasEntMUX(
         conversorBool(de0),
         conversorBool(de1),
         conversorBool(dc0)
+    ))
+}
+
+// executar MUX de quatro entradas
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".quatro-dig").addEventListener("click", quatroMult);
+});
+
+function quatroMult(){
+    let qe0 = document.getElementById("q-e0").value
+    let qe1 = document.getElementById("q-e1").value
+    let qe2 = document.getElementById("q-e2").value
+    let qe3 = document.getElementById("q-e3").value
+    let qc1 = document.getElementById("q-c1").value
+    let qc0 = document.getElementById("q-c0").value
+    console.log(qe0, qe1, qe2, qe3, qc1, qc0)
+    return console.log(quatroEntMUX(
+        conversorBool(qe0),
+        conversorBool(qe1),
+        conversorBool(qe2),
+        conversorBool(qe3),
+        conversorBool(qc1),
+        conversorBool(qc0)
+    ))
+}
+
+// executar MUX de oito entradas
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".oito-dig").addEventListener("click", oitoMult);
+});
+
+function oitoMult(){
+    let oe0 = document.getElementById("o-e0").value
+    let oe1 = document.getElementById("o-e1").value
+    let oe2 = document.getElementById("o-e2").value
+    let oe3 = document.getElementById("o-e3").value
+    let oe4 = document.getElementById("o-e4").value
+    let oe5 = document.getElementById("o-e5").value
+    let oe6 = document.getElementById("o-e6").value
+    let oe7 = document.getElementById("o-e7").value
+    let oc2 = document.getElementById("o-c2").value
+    let oc1 = document.getElementById("o-c1").value
+    let oc0 = document.getElementById("o-c0").value
+    let oen = document.getElementById("o-en").value
+    console.log(oe0, oe1, oe2, oe3, oe4, oe5, oe6, oe7, oc2, oc1, oc0, oen);
+    return console.log(quatroEntMUX(
+        conversorBool(oe0),
+        conversorBool(oe1),
+        conversorBool(oe2),
+        conversorBool(oe3),
+        conversorBool(oe4),
+        conversorBool(oe5),
+        conversorBool(oe6),
+        conversorBool(oe7),
+        conversorBool(oc2),
+        conversorBool(oc1),
+        conversorBool(oc0),
+        conversorBool(oen)
     ))
 }
 
